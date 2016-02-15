a89c3e2495eaa9f73d895d5e2d9ff041b458567f
Ismael Castiñeira <ismael.castineira@students.turkuamk.fi>
2016-02-14 23:55:07 -0600

    Added process and scheduling basics

--- CutreOS/src/Process.java
+++ CutreOS/src/Process.java
@@ -2,8 +2,29 @@
  * Created by ismael on 2/14/16.
  */
 public class Process {
+    public enum Status{
+        RUNNING, BLOCKED, READY, FINISHED
+    }
+
     String name;
     int pid;
 
+    int expected_runtime;
+    int running_time;
+    int blocked_time;
+    int ready_time;
 
+    int last_running_time;
+    int last_blocked_time;
+    int last_ready_time;
+
+
+
+    Status current;
+
+
+    public Process (String name, int expected_runtime){
+        this.name = name;
+        this.expected_runtime = expected_runtime;
+    }
 }
---
+++ CutreOS/src/Scheduling.java
@@ -0,0 +1,24 @@
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by ismael on 2/14/16.
+ */
+public class Scheduling {
+
+    private List<Process> allProcesses = new LinkedList<Process>();
+    private List<Process> ready = new LinkedList<Process>();
+    private List<Process> blocked = new LinkedList<Process>();
+
+    private Process running;
+
+
+
+    public newProcess (String name, int expected_runtime){
+        //blah
+    }
+
+    
+    private static class FIFO {
+    }
+}
