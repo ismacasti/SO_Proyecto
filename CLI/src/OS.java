c1b028ba38aa665928110ac821a4e1ec130dddd8
Ismael Castiñeira <ismael.castineira@edu.turkuamk.fi>
2016-02-17 00:39:00 -0600

    Added adding processes to the kernel

--- CLI/src/OS.java
+++ CLI/src/OS.java
@@ -6,15 +6,25 @@ import java.util.LinkedList;
 /**
  * Created by ismael on 2/15/16.
  */
-public class main {
+public class OS {
+    CutreOS kernel;
     public static void main(String[] args) throws IOException{
         if (args.length == 1) {
             Parser p = new Parser(args[0]);
             LinkedList<ProcessParsed> procs = p.parse();
-            System.out.println("thanks");
+            new OS(procs);
         } else {
             System.out.println("Use only 1 parameter, the path for the input file");
             System.exit(-1);
         }
+    }
+
+    public OS(LinkedList<ProcessParsed> list){
+        kernel = new CutreOS();
+        int i = 0;
+        for(ProcessParsed p: list){
+            kernel.newProcess("Process " + Integer.toString(i), p.getArriveTime(), p.getEstimatedTime(), p.getNextState() );
+        }
+
     }
 }
